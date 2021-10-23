ac53add9feceafad0d522352bc3320d29ce34a50
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-10-23 12:14:18 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def drop(ctx):
 @bot.command(name='meme', help='Generates a random meme.')
 async def plsmeme(ctx):
     content = get("https://meme-api.herokuapp.com/gimme").text
-    data = json.loads(content,)
+    data = json.loads(content)
     meme = discord.Embed(title=f"{data['title']}", Color = discord.Color.random()).set_image(url=f"{data['url']}")
     
     await ctx.reply(embed=meme)
