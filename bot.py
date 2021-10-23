110261a08acbd9a931ab4493a33ac73981cc3b14
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-10-23 10:53:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ client = discord.Client()
 bot = commands.Bot(command_prefix='.')
 
 
-@client.event
+@bot.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
     print(
