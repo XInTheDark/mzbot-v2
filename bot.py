608318b8a8b915fa05d7af1777256ac29372d89d
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-11-14 10:52:53 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1143,7 +1143,8 @@ async def dmnitro(ctx, amount: int):
 @bot.command(name='membercount', aliases=['mc', 'members'])
 async def mc(ctx):
     # count = ctx.guild.member_count
-    count = ctx.message.guild.member_count
+    guild1 = bot.get_guild(ctx.guild.id)
+    count = guild1.member_count
     embed = discord.Embed(title=f"**Member Count**", description=f"""Member count for {ctx.guild.name}:
 `{count}` members""")
     await ctx.reply(embed=embed)
