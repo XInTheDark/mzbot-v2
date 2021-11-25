7ee7b7ba9ba5ed4eb8fe8989ad5b3e2804c4582b
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-11-25 18:42:35 +0800

    Create mzhelp.py

---
+++ mzhelp.py
@@ -0,0 +1,40 @@
+helpcmd = {
+    'meme': "Generates a random meme from reddit.",
+    'donate': "Donate me pls",
+    'nitro': "Generates random nitro code, I promise 100% real real ;)",
+    'dice': "Random dice.",
+    'credits': "Credits.",
+    'ticket': "Opens a ticket... the fast(er) way.",
+    ['delete', 'tclose', 'tdelete']: "Deletes your ticket.",
+    'afk': "Yes! The popular AFK command!",
+    ['about', 'info']: "Some boring system information.",
+    'ping': "Network latency... But you wouldn't notice the difference.",
+    'updates': "Shows the latest update logs!",
+    'dmnitro': "DMs you any amount of nitro codes.",
+    ['membercount', 'mc']: "Member count of the server.",
+    ['snipe', 'sniper']: "Snipes a recently deleted message.",
+    ['editsnipe', 'esnipe']: "Edit snipes a recently edited message.",
+    ['dw']: "{For giveaways only} how a drop works. **Requires Permission(s): Manage Server**",
+    'mute': "Mutes a member. **Requires Permission(s): Manage Server**",
+    'unmute': "Unmutes a member. **Requires Permission(s): Manage Server**",
+    'kick': "Kicks a member. **Requires Permission(s): Kick Members**",
+    'ban': "Bans a member. **Requires Permission(s): Ban Members**",
+    'unban': "Unbans a member. **Requires Permission(s): Ban Members**",
+    'gg': "{For giveaways only} GG, stay in that server. **Requires Permission(s): Manage Server**",
+    'tips': "{For giveaways only} Tips to win giveaways! **Requires Permission(s): Manage Server**",
+    'claimed': "Logs a member's claim in claim logs. **Requires Permission(s): Administrator**",
+    'setclaimschannel': "Sets claim channel for this server. **Requires Permission(s): Administrator**",
+    'won': "{For giveaways only} Shows who won last giveaway. **Requires Permission(s): Manage Server**",
+    'setproofschannel': "Set proof channel for won command. **Requires Permission(s): Administrator**",
+    'spam': "'Spams' a certain amount of the same message in the current channel. **Requires Permission(s): Administrator**",
+    'dmspam': "Spams a certain amount of the same message... But in someone's DMs. You can opt out of this by using '.optout_spam'. **Requires Permission(s): Administrator**",
+    'dmspam_force': "FORCE DM spams someone regardless of whether they opted out! **Requires Permission(s): Administrator**",
+    'lockall': "MASS LOCKDOWNS all the channels! **Requires Permission(s): Manage Channels**",
+    'slowmode': "Sets the slowmode. **Requires Permission(s): Manage Channels**",
+    'purge': "Purges messages. Avoids pinned messages. **Requires Permission(s): Manage Messages**",
+    ['timer', 'countdown']: "Sets a countdown timer. **Requires Permission(s): Administrator**"
+}
+
+
+def helpcmd():
+  return helpcmd
