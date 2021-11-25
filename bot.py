eda092da2b396bf5bb3877debcecda7c1e28e570
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-11-25 19:51:19 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -215,8 +215,8 @@ None
 
 **You may use `.help <command>` for help on that command.**"""
     
-    helpdict = mzhelp.helpcmd()
-    usagedict = mzhelp.helpusage()
+    helpdict = dict(mzhelp.helpcmd())
+    usagedict = dict(mzhelp.helpusage())
     
     if cmd is None:
         embed = discord.Embed(title="Help Page", description=response, color=0x00ff08)
