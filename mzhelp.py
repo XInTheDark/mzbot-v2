21fd1ebd278d16aedf369c99f881d768a6b38a10
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-11-25 19:34:16 +0800

    Update mzhelp.py

--- mzhelp.py
+++ mzhelp.py
@@ -5,16 +5,16 @@ helpcmd = {
     'dice': "Random dice.",
     'credits': "Credits.",
     'ticket': "Opens a ticket... the fast(er) way.",
-    ['delete', 'tclose', 'tdelete']: "Deletes your ticket.",
+    ('delete', 'tclose', 'tdelete'): "Deletes your ticket.",
     'afk': "Yes! The popular AFK command!",
-    ['about', 'info']: "Some boring system information.",
+    ('about', 'info'): "Some boring system information.",
     'ping': "Network latency... But you wouldn't notice the difference.",
     'updates': "Shows the latest update logs!",
     'dmnitro': "DMs you any amount of nitro codes.",
-    ['membercount', 'mc']: "Member count of the server.",
-    ['snipe', 'sniper']: "Snipes a recently deleted message.",
-    ['editsnipe', 'esnipe']: "Edit snipes a recently edited message.",
-    ['dw']: "{For giveaways only} how a drop works. **Requires Permission(s): Manage Server**",
+    ('membercount', 'mc'): "Member count of the server.",
+    ('snipe', 'sniper'): "Snipes a recently deleted message.",
+    ('editsnipe', 'esnipe'): "Edit snipes a recently edited message.",
+    ('dw'): "{For giveaways only} how a drop works. **Requires Permission(s): Manage Server**",
     'mute': "Mutes a member. **Requires Permission(s): Manage Server**",
     'unmute': "Unmutes a member. **Requires Permission(s): Manage Server**",
     'kick': "Kicks a member. **Requires Permission(s): Kick Members**",
@@ -32,7 +32,7 @@ helpcmd = {
     'lockall': "MASS LOCKDOWNS all the channels! **Requires Permission(s): Manage Channels**",
     'slowmode': "Sets the slowmode. **Requires Permission(s): Manage Channels**",
     'purge': "Purges messages. Avoids pinned messages. **Requires Permission(s): Manage Messages**",
-    ['timer', 'countdown']: "Sets a countdown timer. **Requires Permission(s): Administrator**"
+    ('timer', 'countdown'): "Sets a countdown timer. **Requires Permission(s): Administrator**"
 }
 
 helpusage = {
@@ -42,16 +42,16 @@ helpusage = {
     'dice': "`.dice <amount> <number of sides>`",
     'credits': "`.credits`",
     'ticket': "`.ticket`",
-    ['delete', 'tclose', 'tdelete']: "`.delete|tclose|tdelete`",
+    ('delete', 'tclose', 'tdelete'): "`.delete|tclose|tdelete`",
     'afk': "`.afk [reason (default 'AFK')]`",
-    ['about', 'info']: "`.about|info`",
+    ('about', 'info'): "`.about|info`",
     'ping': "`.ping`",
-    ['updates', 'update']: "`.updates|update`",
+    ('updates', 'update'): "`.updates|update`",
     'dmnitro': "`.dmnitro <amount>`",
-    ['membercount', 'mc']: "`.membercount|mc`",
-    ['snipe', 'sniper']: "`.snipe [index]`",
-    ['editsnipe', 'esnipe']: "`.editsnipe|esnipe [index]`",
-    ['dw']: "`.dw`",
+    ('membercount', 'mc'): "`.membercount|mc`",
+    ('snipe', 'sniper'): "`.snipe [index]`",
+    ('editsnipe', 'esnipe'): "`.editsnipe|esnipe [index]`",
+    ('dw'): "`.dw`",
     'mute': "`.mute <discord.Member>`",
     'unmute': "`.unmute <discord.Member>`",
     'kick': "`.kick <discord.Member> [reason (default 'None')]`",
@@ -69,7 +69,7 @@ helpusage = {
     'lockall': "`.lockall`",
     'slowmode': "`.slowmode <seconds>`",
     'purge': "`.purge <limit>`",
-    ['timer', 'countdown']: "`.timer|countdown <time> [timer name (defaults to none)]`"
+    ('timer', 'countdown'): "`.timer|countdown <time> [timer name (defaults to none)]`"
 }
 
 
