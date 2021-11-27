b20444e1916e7de43a7e1a7251959ef5e676bf86
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2021-11-27 15:55:45 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -154,13 +154,13 @@ async def on_message_edit(old, new):
         
 @bot.command(name='update', aliases=['updates', 'log', 'logs', 'announcements', 'notes'])
 async def updatelog(ctx):
-    message = """New Update: 25/11/2021
+    message = """New Update: 27/11/2021
+- New `.whois` command!
 - **HUGE** revamp to the HELP command! You can now use '.help <command>'!!
 - **NEW COMMAND:** `.timer`!!
 (Archive)
 - **New commands! `.snipe` and `.editsnipe`, it works similarly to Apollo bot's except you can't specify a channel yet. That will be added later.**
 - **New option for `.addrole`: `.addrole all` which adds a role to everyone in the server.** 
-- Coming Soon: More complex help command, like '.help <command>' and '.help <Category>'
 - Coming Soon: Developer mode
 - Coming Later: Anti Nuke mode, maybe can be set up.
 - Coming Later: Currency system, may be global
@@ -192,6 +192,7 @@ async def help(ctx, cmd=None):
 - membercount
 - snipe
 - editsnipe
+- whois
 **Requires permissions:**
 - dw
 - mute
