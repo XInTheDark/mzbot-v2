e861542bfc3f245c6249d17a48bae1d4dc5865b1
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-06-15 11:55:43 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ async def on_ready():
 
     owner_user = await bot.fetch_user(ownerid)
     channel = await owner_user.create_dm()
-    computer_name = os.environ['USER']
+    computer_name = os.environ['USERNAME']
     embed = discord.Embed(title="**MZ Bot build succeeded**", description=f"MZ Bot started at <t:{int(launch_time.timestamp())}:f>\n"
                                                                           f"Host user name: {computer_name}\nEnvironment: {os.environ['DESKTOP_SESSION']}",
                           color=0x00ff00)
