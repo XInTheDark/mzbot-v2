b1060319feb0736404fe600245ad9d30a4c6d5d0
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-09-05 20:54:49 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ import urllib.request
 import re
 import speedtest
 import roblox
+import keep_alive
 # import io
 # import aiohttp
 
@@ -2550,5 +2551,7 @@ async def lastmention(ctx, limit: int = 10000):
             return
 
     await ctx.reply(f"I could not find any mention of you in the last `{limit}` messages.")
+
+keep_alive.keep_alive()  # keep bot alive
 
 bot.run(TOKEN)
