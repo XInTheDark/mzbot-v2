c52e71f86f6acb046e331a1d873cfcd8b01c3fa9
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2022-11-24 13:28:11 +0800

    Added replit support again --- Added Firebase support ( firebaseconfig.py, database.rules.json, requirements.txt )

--- replit.nix
+++ replit.nix
@@ -1,8 +1,18 @@
 { pkgs }: {
-    deps = [
-        pkgs.python39Packages.poetry
-        pkgs.python39Packages.pip
-        pkgs.bashInteractive
-        pkgs.python39
+  deps = [
+    pkgs.python38Full
+  ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
     ];
+    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
+    LANG = "en_US.UTF-8";
+  };
 }
