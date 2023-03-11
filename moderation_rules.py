942b55b8cc006ee6a47c53ba03721788e0710aac
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-03-11 20:57:15 +0800

    Add openAI moderate feature (free of charge for API)

--- moderation_rules.py
+++ moderation_rules.py
@@ -1,9 +1,17 @@
 """Special moderation rules"""
 import discord  # discord.py
 import asyncio
+import openai, os  # OpenAI API for moderation
+
+def openAIinit(envName="OPENAI_API_KEY"):
+    openai.api_key = os.getenv(envName)
+    return openai.api_key  # returns None if key not found.
+
+openAIinit()
 
 """Constants"""
 whitelist_servers = [996407200161472542, 1076679987773591643]
+openai_moderate_servers = [996407200161472542, 1083361948181213294] # servers to run openAI's moderation endpoint on
 
 # Spam command related
 spam_ban = [726356086176874537]  # banned from using spam commands
@@ -15,10 +23,15 @@ async def moderate(message: discord.Message):
     # This is where you can add your moderation rules.
 
     msg = message.content
-    if "owo" in msg and "<@926410988738183189>" in msg:  # owner ID
-        await message.delete()  # first delete this message,
-        await asyncio.sleep(1)  # OwO bot is laggy, perhaps need a long(er) delay
-        # If OwO follows up with a message, we delete that message as well.
-        new_msg = [m async for m in message.channel.history(limit=1)][0]
-        if new_msg.author.id == 408785106942164992:  # OwO bot ID
-            await new_msg.delete()
+    
+    # OpenAI moderation
+    if message.guild.id in openai_moderate_servers:
+        response: bool = openai.Moderation.create(
+            input=msg
+        )["results"][0]["flagged"]
+        
+        if response:
+            await message.delete()
+            return True
+        
+    
