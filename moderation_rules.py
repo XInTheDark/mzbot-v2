9f1136533614929c408a28c7faf457469952ee23
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2023-03-23 12:09:02 +0800

    Revert "OpenAI Moderation" See Issue #20.

--- moderation_rules.py
+++ moderation_rules.py
@@ -1,13 +1,7 @@
 """Special moderation rules"""
 import discord  # discord.py
 import asyncio
-import openai, os  # OpenAI API for moderation
 
-def openAIinit(envName="OPENAI_API_KEY"):
-    openai.api_key = os.getenv(envName)
-    return openai.api_key  # returns None if key not found.
-
-openAIinit()
 
 """Constants"""
 whitelist_servers = [996407200161472542, 1076679987773591643]
@@ -23,16 +17,4 @@ async def moderate(message: discord.Message):
     # This is where you can add your moderation rules.
 
     msg = message.content
-    
-    # OpenAI moderation
-    if message.guild.id in openai_moderate_servers\
-            and not message.author.guild_permissions.administrator:
-        response: bool = openai.Moderation.create(
-            input=msg
-        )["results"][0]["flagged"]
-        
-        if response:
-            await message.delete()
-            return True
-        
     
